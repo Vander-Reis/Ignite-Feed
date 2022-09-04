d8a8773b5f7208e7325b6b23bdb1fef17c3234cb
Vander-Reis <vanderreis2017@outlook.com>
2022-09-04 16:17:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Header';
+import Post from './Post';
+import './styles.css'
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+      <Post />
+    </div>
+  )
+}
