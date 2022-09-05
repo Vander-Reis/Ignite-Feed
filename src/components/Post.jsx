e25d8a6639a312669907e23c0feaedf42cd82afa
Vander-Reis <vanderreis2017@outlook.com>
2022-09-05 15:43:37 -0300

    finalização dos components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/Vander-Reis.png"/>
+                    <Avatar src="https://github.com/Vander-Reis.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Vander Reis</strong>
                         <span>Web Developer</span>
@@ -24,6 +26,24 @@ export function Post() {
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p>👉{' '}<a href="#">jane.design/doctorcare</a> </p>
                 <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+
+               <footer>
+                    <button type='submit'>Publicar</button>
+               </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment/>
+                <Comment/>
+                <Comment/>
             </div>
         </article>
     );
